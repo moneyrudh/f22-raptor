7c80dc80d7422a0e6d9deac7945a09bcf30fdb5a
Anirudh Ananthasubramaniam <anirudhananth99@gmail.com>
2025-01-26 12:41:19 -0500

    vercel attempt 4

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# install required tools
+npm install -g cmake-js
+
+# set up emscripten
 curl -L https://github.com/emscripten-core/emsdk/archive/main.tar.gz -o emsdk.tar.gz
 tar -xf emsdk.tar.gz
 cd emsdk-main
@@ -6,7 +10,15 @@ cd emsdk-main
 ./emsdk activate latest
 source ./emsdk_env.sh
 cd ..
-mkdir build
-cd build
+
+# create public directory (this is where vercel expects files)
+mkdir -p public
+cd public
+
+# run build commands
 emcmake cmake ..
 emmake make
+
+# ensure files are in public/
+cp f22_game.* ./
+cp -r ../assets ./
